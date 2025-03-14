97aaef281d85c4a51a3eab39eb10b494803355f9
st912341 <71476272+st912341@users.noreply.github.com>
2025-03-15 01:21:55 +0530

    Update urls.py

--- myaudio/myaudio/urls.py
+++ myaudio/myaudio/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import text_to_speech
 
 urlpatterns = [
-    path('text_to_speech/', text_to_speech, name='text_to_speech/'),
+    path('text_to_speech/', text_to_speech, name='text_to_speech'),  # Removed the trailing slash from the name
 ]

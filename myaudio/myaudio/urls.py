97ac1a5e2e88a3a03699f51a0d445a87182814a4
st912341 <71476272+st912341@users.noreply.github.com>
2025-03-15 01:16:11 +0530

    Update urls.py

--- myaudio/myaudio/urls.py
+++ myaudio/myaudio/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import text_to_speech
 
 urlpatterns = [
-    path('index/', text_to_speech, name='indexp/'),
+    path('text_to_speech/', text_to_speech, name='text_to_speech/'),
 ]

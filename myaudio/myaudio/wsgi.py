086f3744d627c640ee5af9452bc6ade97439162e
st912341 <71476272+st912341@users.noreply.github.com>
2025-03-15 01:05:55 +0530

    Update wsgi.py

--- myaudio/myaudio/wsgi.py
+++ myaudio/myaudio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myaudio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myaudio.myaudio.settings')
 
 application = get_wsgi_application()
